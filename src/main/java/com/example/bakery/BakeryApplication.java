ecef34c0de5b8451d309f966164d6b1cedc0c8a1
Sandila Binadith <57577406+sadilabinadith@users.noreply.github.com>
2026-05-09 10:50:59 +0530

    Update BakeryApplication.java

--- src/main/java/com/example/bakery/BakeryApplication.java
+++ src/main/java/com/example/bakery/BakeryApplication.java
@@ -2,9 +2,17 @@ package com.example.bakery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BakeryApplication {
+public class BakeryApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BakeryApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BakeryApplication.class, args);
     }
